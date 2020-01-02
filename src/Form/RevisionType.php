8ecebe854d498b6330323b81e603c3627de22cf1
Seb-back75 <sebastien.l7516@gmail.com>
2020-01-02 14:20:38 +0100

    test

---
+++ src/Form/RevisionType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Revision;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RevisionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('contenu')
+            ->add('name')
+            ->add('created_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Revision::class,
+        ]);
+    }
+}
